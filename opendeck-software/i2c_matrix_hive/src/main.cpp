f20c3d71444579bc12092e13ff4294fa56aee990
Gabriel Oliveira <sowee00@gmail.com>
2023-03-10 22:33:48 -0300

    Writing the hive and worker code

--- opendeck-software/i2c_matrix_hive/src/main.cpp
+++ opendeck-software/i2c_matrix_hive/src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
+#include <Wire.h>
+
+#define i2c_addr 8
+#define msg_size 4
+
+int count = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //ask i2c device for the reading
+  Serial.print("Request number ");
+  Serial.println(count);
+  Wire.requestFrom(i2c_addr, msg_size);
+
+
+  //print the reading
+  while(Wire.available()){
+    char c = Wire.read();
+    Serial.print(c);
+  }
+
+  Serial.println("");
+
+  count++;
+
+  //wait
+  delay(1000);
 }
--- opendeck-software/i2c_matrix_worker/src/main.cpp
+++ opendeck-software/i2c_matrix_worker/src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
+#include <Wire.h>
+
+
+#define i2c_addr 8
+#define msg_size 4
+
+char char0 = 'A';
+char char1 = 'B';
+char char2 = 'C';
+char char3 = 'D';
+
+char char_array [] = {'A', 'B', 'C', 'D'};
+
+void transmit_four();
 
 void setup() {
-  // put your setup code here, to run once:
+  Wire.begin(i2c_addr);
+  Wire.onRequest(transmit_four);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void transmit_four(){
+
+  Wire.write(char0);
+  Wire.write(char1);
+  Wire.write(char2);
+  Wire.write(char3);
+
 }
