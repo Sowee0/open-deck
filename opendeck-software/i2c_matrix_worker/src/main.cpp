bacd14d8fe1dd6fb747da3947e85b7858604846a
Gabriel Oliveira <sowee00@gmail.com>
2023-03-14 21:54:56 -0300

    Moving things around and reorganizing

--- opendeck-software/i2c_matrix_worker/src/main.cpp
+++ opendeck-software/i2c_matrix_worker/src/main.cpp
@@ -9,25 +9,48 @@ char char0 = 'A';
 char char1 = 'B';
 char char2 = 'C';
 char char3 = 'D';
+int ledpin = 13;
 
 char char_array [] = {'A', 'B', 'C', 'D'};
 
-void transmit_four();
-
-void setup() {
-  Wire.begin(i2c_addr);
-  Wire.onRequest(transmit_four);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
 void transmit_four(){
-
+  Serial.println("Request!");
+  digitalWrite(ledpin, HIGH);
   Wire.write(char0);
   Wire.write(char1);
   Wire.write(char2);
   Wire.write(char3);
+  //Wire.write(char_array, msg_size);
+  //Wire.endTransmission(true);
 
+  delay(200);
+  digitalWrite(ledpin, LOW);
+  delay(200);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledpin, OUTPUT);
+
+  digitalWrite(ledpin, HIGH);
+  delay(200);
+  digitalWrite(ledpin, LOW);
+  delay(200);
+  digitalWrite(ledpin, HIGH);
+  delay(200);
+  digitalWrite(ledpin, LOW);
+  delay(200);
+  digitalWrite(ledpin, HIGH);
+  delay(200);
+  digitalWrite(ledpin, LOW);
+  delay(200);
+
+
+  Wire.begin(i2c_addr);
+  Wire.onRequest(transmit_four);
+  Serial.println("Starting!");
+}
+
+void loop() {
+  delay(100);
 }
